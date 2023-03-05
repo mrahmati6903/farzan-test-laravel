f8020698b54d8ed5a223c63413e3c2bb5d8b49e2
mrahmati6903 <mehdirahmati6903@gmail.com>
2023-03-06 01:05:57 +0330

    create seeder for default super admin user

---
+++ database/seeders/DefaultAdminSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class DefaultAdminSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        User::factory()->state([
+            'name'     => 'admin',
+            'password' => Hash::make('1234')
+        ])->create();
+    }
+}
