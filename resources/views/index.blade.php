a36f50f4ecb808bbfe957eaa9b19a4ab6b112831
mrahmati6903 <mehdirahmati6903@gmail.com>
2023-03-06 01:15:31 +0330

    create basic view layout and edit index page

---
+++ resources/views/index.blade.php
@@ -0,0 +1,5 @@
+@extends('layout.layout')
+
+@section('content')
+    <h1>motorbike project</h1>
+@endsection
---
+++ resources/views/layout/layout.blade.php
@@ -0,0 +1,17 @@
+<html>
+
+<head>
+    <title>Motorbike Project</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css">
+</head>
+
+<body>
+<div class="container">
+    @yield('content')
+</div>
+<script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.min.js"></script>
+</body>
+
+</html>
